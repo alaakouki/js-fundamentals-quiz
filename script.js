281f5088a98b82e5b436f709ffe4c5c357fe8d7a
alaakouki <119391046+alaakouki@users.noreply.github.com>
2023-01-19 21:49:10 -0800

    creating HTML, CSS and script JS

---
+++ script.js
@@ -0,0 +1,48 @@
+var quizQuestions = [
+
+{
+
+question: "Commonly used data types Do Not Include:",
+a: "strings",
+b: "booleans",
+c: "alerts",
+d: "numbers",
+correctAnswer: "c"
+
+}, {
+
+question: "The condition in an if / else statement is enclosed with _____.",
+a: "quotes",
+b: "curly brackets",
+c: "parenthsis",
+d: "square brackets",
+correctAnswer: "c"
+
+}, {
+
+question: "Arrays in JavaScript can be used to store _____.",
+a: "numbers and strings",
+b: "other arrays",
+c: "booleans",
+d: "all of the above",
+correctAnswer: "d"
+
+}, {
+
+question: "String values must be enclosed within _____ when being assigned to variables.",
+a: "commas",
+b: "curly brackets",
+c: "quotes",
+d: "parenthesis",
+correctAnswer: "c"
+
+}, {
+    question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+a: "JavaScript",
+b: "terminal/bash",
+c: "for loops",
+d: "console.log",
+correctAnswer: "d"
+}
+
+]
