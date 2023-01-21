4f1334f7b6511ca447bf562fe30b9b2d98420bd4
alaakouki <119391046+alaakouki@users.noreply.github.com>
2023-01-20 23:14:31 -0800

    1st update for script file, css and some changes in HTML

--- script.js
+++ script.js
@@ -45,4 +45,88 @@ d: "console.log",
 correctAnswer: "d"
 }
 
-]
+];
+
+var startQuiz = document.querySelector("#start-btn");
+var secondLeft = 75;
+
+
+
+function beginTimer() {
+  var timerInterval = setInterval(function() {
+
+secondLeft--;
+
+if(secondLeft===0) {
+  clearInterval(timerInterval);
+  // sendMessage();
+}
+  }, 1000);
+}
+
+startQuiz.addEventListener ("click", function {
+  beginTimer();
+});
+
+// function nextQuestion() {
+    
+// }
+
+
+
+
+
+// event.preventDefault();
+
+
+var secondsLeft = 10;
+
+function setTime() {
+  // Sets interval in variable
+  var timerInterval = setInterval(function() {
+    secondsLeft--;
+    timeEl.textContent = secondsLeft + " seconds left till colorsplosion.";
+
+    if(secondsLeft === 0) {
+      // Stops execution of action at set interval
+      clearInterval(timerInterval);
+      // Calls function to create and append image
+      sendMessage();
+    }
+
+  }, 1000);
+}
+
+// Function to create and append colorsplosion image
+function sendMessage() {
+  timeEl.textContent = " ";
+  var imgEl = document.createElement("img");
+  imgEl.setAttribute("src", "images/image_1.jpg");
+  mainEl.appendChild(imgEl);
+
+}
+
+setTime();
+
+
+
+// // Access toggle switch HTML element
+// var themeSwitcher = document.querySelector("#theme-switcher");
+// var container = document.querySelector(".container");
+
+// // Set default mode to dark
+// var mode = "dark";
+
+// // Listen for a click event on toggle switch
+// themeSwitcher.addEventListener("click", function() {
+//   // If mode is dark, apply light background
+//   if (mode === "dark") {
+//     mode = "light";
+//     container.setAttribute("class", "light");
+//   }
+//   // If mode is light, apply dark background 
+//   else {
+//     mode = "dark";
+//     container.setAttribute("class", "dark");
+//   }
+// });
